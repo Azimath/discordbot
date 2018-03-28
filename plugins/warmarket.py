acfbc31b6560760253514239ead18f090f3fcc4b
Azimath <npnickster@gmail.com>
2018-03-28 19:19:07 -0400

    Fixed item count in warmarket, first 'done' version

--- plugins/warmarket.py
+++ plugins/warmarket.py
@@ -21,7 +21,7 @@ async def getDuckListings(triggerMessage):
         
     count = 0
     itemsToBuy = []
-    items = [i for i in itemRequest.json()['payload']['items']['en'] if 'prime' in item['url_name'] and 'primed' not in item['url_name']]
+    items = [i for i in itemRequest.json()['payload']['items']['en'] if 'prime' in i['url_name'] and 'primed' not in i['url_name']]
     
     await client.send_message(triggerMessage.channel, "Beginning ducat search with " + str(len(items)) + " items")
     progressMessage = await client.send_message(triggerMessage.channel, "Progress: 0%")
@@ -29,47 +29,48 @@ async def getDuckListings(triggerMessage):
     lastProgressUpdate = 0
     
     for index, item in enumerate(items):
-		itemInfoRequest = requests.get('https://api.warframe.market/v1/items/'+item['url_name'])
-		if itemInfoRequest.status_code != 200:
-			await client.send_message(triggerMessage.channel, "Item info request error, aborting further ducat search")
-			break
-		
-		itemInfo = itemInfoRequest.json()['payload']['item']
-		
-		ducats = 0
-		if 'set' not in item['url_name']:
-			for setItem in itemInfo['items_in_set']:
-				if 'ducats' in setItem and setItem['_id'] == itemInfo['_id']:
-					ducats = setItem['ducats']
-		else:
-			for setItem in itemInfo['items_in_set']:
-				if 'ducats' in setItem:
-					ducats += setItem['ducats']
-					
-		itemOrdersRequest = requests.get('https://api.warframe.market/v1/items/'+item['url_name']+'/orders')
-		
-		if itemOrdersRequest.status_code != 200:
-			await client.send_message(triggerMessage.channel, "Item orders request error, aborting further ducat search")
-			break
-			
-		itemOrders = itemOrdersRequest.json()['payload']['orders']
-		
-		prices = []
-		for order in itemOrders:
-			if order['order_type'] == 'sell' and order['user']['status'] == 'online' and order['platform'] == 'pc':
-				prices.append(order['platinum'])
-				
-		prices.sort()
-		if len(prices) > 0:
-			if ducats/prices[0] > 8:
-				#print(item['item_name'] + " : " + str(ducats) + " ducats, best: " + str(ducats/prices[0]) + " ducats per plat, median: " + str(ducats/prices[int(len(prices)/2)]) + " ducats per plat")
-				itemsToBuy.append((item['item_name'],ducats,ducats/prices[0],ducats/prices[int(len(prices)/2)]))
-		
-		if index > lastProgressUpdate+5:
-			await client.edit_message(progressMessage, "Progress: {:0.2f}%".format(100.0*index/len(items)))
-			lastProgressUpdate = index
-			
-		time.sleep(0.67)
+        itemInfoRequest = requests.get('https://api.warframe.market/v1/items/'+item['url_name'])
+        if itemInfoRequest.status_code != 200:
+            await client.send_message(triggerMessage.channel, "Item info request error, aborting further ducat search")
+            break
+        
+        itemInfo = itemInfoRequest.json()['payload']['item']
+        
+        ducats = 0
+        if 'set' not in item['url_name']:
+            for setItem in itemInfo['items_in_set']:
+                if 'ducats' in setItem and setItem['_id'] == itemInfo['_id']:
+                    ducats = setItem['ducats']
+        else:
+            for setItem in itemInfo['items_in_set']:
+                if 'ducats' in setItem:
+                    ducats += setItem['ducats']
+                    
+        itemOrdersRequest = requests.get('https://api.warframe.market/v1/items/'+item['url_name']+'/orders')
+        
+        if itemOrdersRequest.status_code != 200:
+            await client.send_message(triggerMessage.channel, "Item orders request error, aborting further ducat search")
+            break
+            
+        itemOrders = itemOrdersRequest.json()['payload']['orders']
+        
+        prices = []
+        for order in itemOrders:
+            if order['order_type'] == 'sell' and order['user']['status'] == 'online' and order['platform'] == 'pc':
+                prices.append(order['platinum'])
+                
+        prices.sort()
+        
+        if len(prices) > 0:
+            if ducats/prices[0] > 8:
+                #print(item['item_name'] + " : " + str(ducats) + " ducats, best: " + str(ducats/prices[0]) + " ducats per plat, median: " + str(ducats/prices[int(len(prices)/2)]) + " ducats per plat")
+                itemsToBuy.append((item['item_name'],ducats,ducats/prices[0],ducats/prices[int(len(prices)/2)]))
+        
+        if index > lastProgressUpdate+5:
+            await client.edit_message(progressMessage, "Progress: {:0.2f}%".format(100.0*index/len(items)))
+            lastProgressUpdate = index
+            
+        time.sleep(0.67)
     
     await client.edit_message(progressMessage, "Progress: complete!")
     
