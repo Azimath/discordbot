7d44123ea8cf970ae455f9d0c9f8b01c7315003a
Nicholas Peters <npnickster@gmail.com>
2019-03-31 20:35:27 -0400

    triggerMessage is case sensitive I guess

--- plugins/uwu.py
+++ plugins/uwu.py
@@ -4,8 +4,8 @@ import discord
 client = None
 
 @commands.registerEventHandler(triggerType="\\messageNoBot", name="dewetethis")
-async def dewetethis(triggermessage):
-    if ("r" in message.content.lower() or "l" in message.content.lower()) and not ("uwu" in message.content.lower() or "owo" in message.content.lower()):
+async def dewetethis(triggerMessage):
+    if ("r" in triggerMessage.content.lower() or "l" in triggerMessage.content.lower()) and not ("uwu" in triggerMessage.content.lower() or "owo" in triggerMessage.content.lower()):
         if (triggerMessage.server.id == "102981131074297856"):
             await client.send_message(triggerMessage.author, "UwU Pwease wewwite youw message without iwwegaw chawactews and/ow add owo ow uwu.")
             await client.delete_message(triggerMessage)
