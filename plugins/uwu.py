7cd67050d7d463bd238c869cfced3aed91dfff1f
Nicholas Peters <npnickster@gmail.com>
2019-03-31 20:26:34 -0400

    Fix the uwuew

--- plugins/uwu.py
+++ plugins/uwu.py
@@ -3,13 +3,7 @@ import discord
 
 client = None
 
-@commands.registerEventHandler(triggerType="\\messageNoBot", name="deweteL")
-@commands.messageHandlerFilter("L", filterType="cqc")
-async def deweteL(triggerMessage):
-    await deweteR(triggerMessage)
-
-@commands.registerEventHandler(triggerType="\\messageNoBot", name="deweteR")
-@commands.messageHandlerFilter("R", filterType="cqc")
+@commands.registerEventHandler(triggerType="\\messageNoBot", name="dewetethis")
 async def dewetethis(triggermessage):
     if ("r" in message.content.lower() or "l" in message.content.lower()) and not ("uwu" in message.content.lower() or "owo" in message.content.lower()):
         if (triggerMessage.server.id == "102981131074297856"):
