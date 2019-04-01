fadc6d1fc94d17030306c84217512d7b583ff31e
Nicholas Peters <npnickster@gmail.com>
2019-03-31 20:32:34 -0400

    Amatuew houw fowgetting cwose pawens

--- plugins/uwu.py
+++ plugins/uwu.py
@@ -7,5 +7,5 @@ client = None
 async def dewetethis(triggermessage):
     if ("r" in message.content.lower() or "l" in message.content.lower()) and not ("uwu" in message.content.lower() or "owo" in message.content.lower()):
         if (triggerMessage.server.id == "102981131074297856"):
-            await client.send_message(triggerMessage.author, "UwU Pwease wewwite youw message without iwwegaw chawactews and/ow add owo ow uwu."
+            await client.send_message(triggerMessage.author, "UwU Pwease wewwite youw message without iwwegaw chawactews and/ow add owo ow uwu.")
             await client.delete_message(triggerMessage)
