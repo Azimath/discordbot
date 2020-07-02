03bc48393057488b087214981de951cee830935d
Kexus <pangolin321@gmail.com>
2020-07-01 22:10:10 -0400

    debug print channel info

--- plugins/booru.py
+++ plugins/booru.py
@@ -114,6 +114,7 @@ async def booru(triggerMessage):
         await triggerMessage.channel.send( "Syntax is `!booru booru_name tag0 ...`\nCurrently supported boorus: " + str(list(functionMap.keys())))
         return
     if (triggerMessage.channel.type is discord.ChannelType.text and not triggerMessage.channel.is_nsfw()) or not (triggerMessage.channel.type is discord.ChannelType.group or triggerMessage.channel.type is discord.ChannelType.private):
+        print(triggerMessage.channel.type is discord.ChannelType.text and not triggerMessage.channel.is_nsfw(), not (triggerMessage.channel.type is discord.ChannelType.group or triggerMessage.channel.type is discord.ChannelType.private),triggerMessage.channel.type is discord.ChannelType.group,  triggerMessage.channel.type is discord.ChannelType.private) 
         tokens.append("rating:safe")
     else:
         tokens.extend(["-young", "-scat","-fart"]) #Anti trash
